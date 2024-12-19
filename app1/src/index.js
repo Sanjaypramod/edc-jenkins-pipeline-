0cf676e11a43fdacaba1e859ac166fa446771780
sanjaypramod <sanjaypramod.p@urolime.com>
2024-12-19 11:57:38 +0530

    added

--- app1/src/index.js
+++ app1/src/index.js
@@ -1,5 +1,5 @@
 const { greet } = require('../../shared/utils');
 const { APP_NAME } = require('../../shared/constants');
 
-console.log(`${greet('App12')} from ${APP_NAME}`);
+console.log(`${greet('App1')} from ${APP_NAME}`);
 
